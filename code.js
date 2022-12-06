30ad1002622586df4ffaa59e951f1a8f9ef6cda4
katiiisha <mendeleevakate2002@gmail.com>
2022-12-07 00:18:20 +0300

    занятие

---
+++ code.js
@@ -0,0 +1,12 @@
+let num1 = +prompt()
+let num2 = +prompt()
+let num3 = +prompt()
+let num4 = +prompt()
+let num5 = +prompt()
+let num6 = +prompt()
+
+let result1 = num1+num2
+let result2 = num3+num4
+let result3 = num5+num6
+
+alert(`Наш результат1: ${result1} Наш результат2: ${result2} Наш результат3: ${result3}`)
---
+++ code1.js
@@ -0,0 +1,38 @@
+// let name1 = prompt("Ваше имя?")
+// let age = prompt ("Ващ возраст?")
+// alert(`Введено имя ${name1%age} Ваш возраст ${age}`)
+
+// let str = "строка"
+// let num = 12 //число
+// let bigint = "большое число"
+// let bool = "true/false"
+// let notValue = "null"
+// let undefined = ""
+ 
+// let num = "asadada"
+// let num2 = 22
+// alert (`resual ${num%num2}`)
+
+// выведет Nаn - не число 
+
+// если мы ставим [+] перед так функцией то js воспринимает это как число в строку
+
+//Операторы сравнения 
+// <=
+//>=
+// == нестрогое сравнение
+// === строгое сравнение 
+//! не
+// && - и 
+//|| - или 
+
+ let num1 = prompt("Введите число")
+ let num2 = prompt ("Введите число")
+ if (num1>num2){
+    alert(`Наибольшее ${num1}`)
+ } else {
+    alert(`Наибольшее ${num2}`)
+ }
+ 
+
+
